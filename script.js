d464f7bf6ff12ca9f1be649ecc0064e08221c6ff
Numen82 <144496810+Numen82@users.noreply.github.com>
2024-07-10 14:26:40 -0400

    Updated js to work with Github

--- script.js
+++ script.js
@@ -1,26 +1,25 @@
-if (document.URL.includes("index.html")) {
-    const scrollers = document.querySelectorAll(".scroller");
+const scrollers = document.querySelectorAll(".scroller");
 
-    if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
-        addAnimation();
-    }
-
-    function addAnimation() {
-        scrollers.forEach((scroller) => {
-            scroller.setAttribute("data-animated", true);
-
-            const scrollerInner = scroller.querySelector(".inner-scroller");
-            const scrollerContent = Array.from(scrollerInner.children);
-            console.log(scrollerContent)
-
-            scrollerContent.forEach((item) => {
-                const duplicatedItem = item.cloneNode(true);
-                duplicatedItem.setAttribute("aria-hidden", true);
-                scrollerInner.appendChild(duplicatedItem);
-            });
-        });
-    }
+if (!window.matchMedia("(prefers-reduced-motion: reduce)").matches) {
+    addAnimation();
 }
+
+function addAnimation() {
+    scrollers.forEach((scroller) => {
+        scroller.setAttribute("data-animated", true);
+
+        const scrollerInner = scroller.querySelector(".inner-scroller");
+        const scrollerContent = Array.from(scrollerInner.children);
+        console.log(scrollerContent)
+
+        scrollerContent.forEach((item) => {
+            const duplicatedItem = item.cloneNode(true);
+            duplicatedItem.setAttribute("aria-hidden", true);
+            scrollerInner.appendChild(duplicatedItem);
+        });
+    });
+}
+
 
 if (document.URL.includes("gallery.html")) {
     let e = false
