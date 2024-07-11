a39cb7994137240da6ceb845d17aa3d82b88cad3
Numen82 <144496810+Numen82@users.noreply.github.com>
2024-07-11 13:58:30 -0400

    Gallery Draft
    Added some stylistic choices: moving the gallery above the social and logo changed from "Langley Table Tennis" => "Langley TTC";
    Created the Gallery Page. Extra work needs to be done so that the image will enlarge.

--- script.js
+++ script.js
@@ -22,9 +22,34 @@ function addAnimation() {
 
 
 if (document.URL.includes("gallery.html")) {
-    let e = false
-    if (!e) {
-        console.log("hehe");
-        e = true;
+    const gallery = document.getElementsByClassName("gal-holder")[0];
+    const imgBlock = document.getElementsByClassName("image")[0];
+    var docFrag = document.createDocumentFragment();
+
+    //https://stackoverflow.com/questions/29488371/how-to-add-a-list-of-images-to-the-document-from-an-array-of-urls 
+    //The stackoverflow above was very useful
+    for (var i = 1; i <= 9; i++) {
+        docFrag.appendChild(setImg(i, imgBlock.cloneNode(true)));
+        console.log();
     }
+
+    console.log(docFrag);
+    console.log(imgBlock);
+    //imgBlock.style.display = "block";
+    
+    gallery.appendChild(docFrag);
+}
+
+function setImg(num, image) {
+    image.style.display = "block";
+    image.style.backgroundImage = "url(img/TTCImgs/club\\ \\(" +  num + "\\).jpg)";
+    image.setAttribute("id", "image " + num)
+    //image.setAttribute("onclick", "enlarge()")
+    return image;
+}
+
+//TODO: Do this later with help from this
+//https://www.geeksforgeeks.org/toggle-class-by-adding-the-class-name-when-element-is-clicked-and-remove-when-clicked-outside/
+function enlarge() {
+    image.style.transform = "translate(30px)";
 }
